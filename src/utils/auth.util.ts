4a4e1560a2a198fb5bdd6f15b501b79e644b37a8
Claudia <claudia.ortiz@uc.cl>
2024-12-05 22:14:38 -0300

    update auth.util for generate token

---
+++ src/utils/auth.util.ts
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const secret = process.env.JWT_SECRET || "your_secret_key";
+export const generateAccessToken = (
+  email: string,
+  uid: string,
+  expiresIn = "1h"
+) => {
+  return jwt.sign({ email, uid }, secret, {
+    expiresIn,
+  });
+};
+
+export const verifyAccessToken = (token: string) => {
+    return jwt.verify(token, secret) as jwt.JwtPayload;
+}
