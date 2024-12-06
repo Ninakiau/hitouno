823c9189a2accbe8c31ad1d491cc8907b7513195
Claudia <claudia.ortiz@uc.cl>
2024-12-05 22:12:19 -0300

    database configurations

---
+++ src/config/database.ts
@@ -0,0 +1,10 @@
+import pg from 'pg';
+
+const {Pool} = pg;
+
+const connectionString= process.env.DATABASE_URL
+
+export const pool = new Pool({
+    connectionString,
+    allowExitOnIdle: true
+});
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,53 @@
+import "dotenv/config"
 import express from "express";
+import rateLimit from "express-rate-limit";
 import userRouter from "./routes/user.route";
 import authRouter from "./routes/auth.route";
 import catRouter from "./routes/cat.route";
+import swaggerUi from "swagger-ui-express";
+import openapiSpecification from "./config/swagger";
+import { loggerMiddleware } from "./middlewares/logger.middleware";
+import { httpErrorHandler } from "./middlewares/httpErrorHandler";
+import { pool } from "./config/database";
 
 const app = express();
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true})); // Para formularios
+app.use(express.urlencoded({ extended: true }));
 
+// Configurar el limitador
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 100, // Límite de 100 peticiones por IP
+    message:
+        "Demasiadas solicitudes desde esta IP, por favor inténtalo más tarde.",
+    standardHeaders: true, // Informa el límite en las cabeceras `RateLimit-*`
+    legacyHeaders: false, // Desactiva las cabeceras `X-RateLimit-*`
+});
+
+app.use(limiter);
+
+app.use(loggerMiddleware);
+app.use(httpErrorHandler); // Middleware para manejar errores  
 app.use("/api/v1/", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/cats", catRouter);
 
-app.listen(PORT, () => {
-    console.log(`Servidor andando en el puerto:  ${PORT}`);
-});
 
+const main = async () => {
+    try {
+        const { rows } = await pool.query("SELECT NOW()");
+        console.log(rows[0].now, "db conectada!");
+        app.listen(PORT, () => {
+            console.log("Servidor andando en el puerto: " + PORT);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+main();
